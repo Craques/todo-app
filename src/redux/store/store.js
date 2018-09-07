2a698f1a11d4a8e2197765009168023985042745
craques <cmuzhanye@gmail.com>
2018-09-07 16:43:52 +0200

    staring the todo app

---
+++ src/redux/store/store.js
@@ -0,0 +1,24 @@
+import {combineReducers, applyMiddleware, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+
+const todoReducer = (state={todos: []}, action)=>{
+	switch(action.type){
+		case 'ADD_TODO':
+			return {...state, todos: [...state.todos, action.todo]}
+		default:
+			return state
+	}
+}
+
+const rootReducer = combineReducers({
+	todos: todoReducer
+})
+
+const enhancer = compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension ? window.devToolsExtension(): f => f
+)
+
+const store = createStore(rootReducer, enhancer)
+
+export default store

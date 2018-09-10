e51f37ea9c32874317e0d9c88e5d55ac6dc33d69
craques <cmuzhanye@gmail.com>
2018-09-10 19:33:41 +0200

    added github link

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,6 +3,9 @@ import {AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {blueGrey} from '@material-ui/core/colors';
 import {Create} from '@material-ui/icons';
+import {FaGithub} from 'react-icons/fa'
+//Github link
+//https://github.com/Craques/todo-app
 
 const theme = createMuiTheme({
 	palette: {
@@ -25,6 +28,12 @@ class Header extends Component{
 						<Typography variant="title" color="inherit">
 							 Todo App
 						</Typography>
+						<Typography style={{flex: 1}}/>
+						<a href="https://github.com/Craques/todo-app">
+							<IconButton color='inherit'>
+								<FaGithub/>
+							</IconButton>
+						</a>
 					</Toolbar>
 				</AppBar>
 			</MuiThemeProvider>
